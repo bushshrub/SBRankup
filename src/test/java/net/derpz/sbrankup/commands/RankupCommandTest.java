60b0bf448b37d9cbbf8dae3c10519b7a954ee3c9
xiurobert <xiurobert@gmail.com>
2017-09-16 18:31:35 +0800

    Wow I made a test!

---
+++ src/test/java/net/derpz/sbrankup/commands/RankupCommandTest.java
@@ -0,0 +1,15 @@
+package net.derpz.sbrankup.commands;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by xiurobert on 16-Sep-17.
+ */
+public class RankupCommandTest {
+    @Test
+    public void onCommand() throws Exception {
+    }
+
+}
